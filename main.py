120e504aabb9d7af2283ce1bcf87b0f466bf74ff
Daulet <onlygoodness32@gmail.com>
2025-05-28 17:22:49 +0500

    add dockstrings to functions

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ CITY_COORDINATES = {
 }
   
 def fetch_weather_data(latitude, longitude):
+    """
+    Выполняет запрос к API погоды с указанными координатами.
+    
+    :param latitude: Широта города
+    :param longitude: Долгота города
+    :return: Ответ от API в формате Response
+    """
     url = (f'https://api.open-meteo.com/v1/forecast'
            f'?latitude={latitude}'
            f'&longitude={longitude}'
@@ -14,6 +21,12 @@ def fetch_weather_data(latitude, longitude):
     return requests.get(url, verify=False)
 
 def parse_weather_data(response):
+    """
+    Обрабатывает ответ от API и извлекает данные о текущей погоде.
+    
+    :param response: Ответ от API в формате Response
+    :return: Словарь с информацией о погоде или ошибкой
+    """
     if response.status_code==200:
         data=response.json()
         weather = data.get('current_weather', {})
@@ -27,11 +40,24 @@ def parse_weather_data(response):
         return {'Ошибка': 'Не удалось получить данные о погоде'}
 
 def get_city_coordinates(city_name, city_coordinates=None):
+    """
+    Возвращает координаты города по его названию.
+    
+    :param city_name: Название города
+    :param city_coordinates: Пользовательский словарь с координатами (опционально)
+    :return: Кортеж с координатами или None
+    """
     if city_coordinates is None:
         city_coordinates = CITY_COORDINATES
     return city_coordinates.get(city_name)
 
 def get_weather_by_city(city_name):
+    """
+    Получает данные о текущей погоде в указанном городе.
+    
+    :param city_name: Название города
+    :return: Словарь с информацией о погоде или ошибкой
+    """
     coordinates = get_city_coordinates(city_name)
     if  not coordinates:
         return {"Ошибка": f"City '{city_name}' не найден в базе данных координат." }
